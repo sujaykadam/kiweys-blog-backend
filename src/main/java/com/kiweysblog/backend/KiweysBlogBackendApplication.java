2de3a204370d76f0f8ed8ee8c4b79bbd014c90fe
sujaykadam <sujay.kadam@yocket.in>
2022-10-19 14:10:38 +0530

    Initial Commit

--- src/main/java/com/kiweysblog/backend/KiweysBlogBackendApplication.java
+++ src/main/java/com/kiweysblog/backend/KiweysBlogBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KiweysBlogBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KiweysBlogBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KiweysBlogBackendApplication.class, args);
+    }
 
 }
--- src/test/java/com/kiweysblog/backend/KiweysBlogBackendApplicationTests.java
+++ src/test/java/com/kiweysblog/backend/KiweysBlogBackendApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class KiweysBlogBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
