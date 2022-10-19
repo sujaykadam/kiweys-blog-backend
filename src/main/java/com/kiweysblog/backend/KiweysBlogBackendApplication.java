fb506b5f792deba9fe5b6e737dfeded90a78ba65
sujaykadam <sujay.kadam@yocket.in>
2022-10-19 14:10:22 +0530

    Initial commit

---
+++ src/main/java/com/kiweysblog/backend/KiweysBlogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kiweysblog.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiweysBlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiweysBlogBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiweysblog/backend/KiweysBlogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiweysblog.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiweysBlogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
