5a385aff6d7565208c8a5c65af25a5b2ab36b5b7
sujaykadam <sujay.kadam@yocket.in>
2022-12-17 12:55:12 +0530

    removed unused validation import

--- src/main/java/com/kiweysblog/backend/payload/UserDto.java
+++ src/main/java/com/kiweysblog/backend/payload/UserDto.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
